19e7cec8395704bf6986bdf29696f147660ab539
Robostangs-Software <122229138+Robostangs-Software@users.noreply.github.com>
2025-03-11 20:17:40 -0400

    new setpoints

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -53,12 +53,12 @@ public final class Constants {
     public static final double kGearboxRotationsToMechanismMeters = 1d;
 
     // Deploy Constants
-    public static final double kMaxExtension = 130;
+    public static final double kMaxExtension = 140;
     public static final double kExtensionDutyCycle = 0.5;
 
     // Reel Constants
     public static final double kReelDutyCycle = -0.5;
-    public static final double kReelSafe = 50;
+    public static final double kReelSafe = 20;
 
 
   }
